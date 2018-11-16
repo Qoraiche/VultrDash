219cb509285a5a33b3996d380ffc3f49b4d0eab4
Yassine <qoraicheofficiel@hotmail.com>
2018-11-16 17:51:50 +0100

    formatting connection error response

--- resources/views/errors/connection.blade.php
+++ resources/views/errors/connection.blade.php
@@ -5,12 +5,14 @@
     <div class="container text-center">
       <div class="display-1 text-muted mb-5"><i class="fe fe-alert-circle"></i></div>
       <h1 class="h2 mb-4">Unable to Connect to Vultr API</h1>
-  <p class="h4 text-muted font-weight-normal mb-2">The pae will load in <span class="remaining-sec">8</span> seconds…</p>
+  <p class="h4 text-muted font-weight-normal mb-2">The pae will load in <span class="remaining-sec">10</span> seconds…</p>
 
       @if ( !empty($error) )
 
         <span class="d-block mb-4">
-            <small><strong>Error: </strong>{{ $error }}</small>
+            <small><strong>Error: </strong>
+              {!! str_replace('response', '</br><b>Response</b>', $error) !!}
+            </small>
         </span>
 
       @endif
@@ -31,7 +33,7 @@
 
     jQuery(function ($) {
 
-        var seconds = 8,
+        var seconds = 10,
             display = $('.remaining-sec');
 
         startCountdown(seconds, display);
@@ -42,7 +44,7 @@
 
            window.location = '{{ url('/refresh') }}';
 
-       }, 8000);
+       }, 10000);
 
   });
 
